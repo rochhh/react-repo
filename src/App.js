aa4d6607b102ac0068842c6e3344e5f2f54025d3
Roch <rochishnu7@gmail.com>
2023-06-10 01:46:01 +0530

    CRUD for books done fine with json-server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+import axios from 'axios';
 
 const App = () => {
   
   const [books , setBooks] = useState([]);
 
-  const deleteBook = (id) => {
+  const deleteBook = async (id) => {
+
+    const response = await axios.delete(`http://localhost:3001/books/${id}`)
+
     const updatedBooks = books.filter( (book) => {
         return book.id !== id;
     })
@@ -14,11 +18,13 @@ const App = () => {
     return setBooks(updatedBooks);
   }
 
-  const editBook = (id , title) => {
-    
+  const editBook = async (id , title) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}` , {
+      title:title
+    } )
     const updatedBooks = books.map( (book) => {
       if (book.id === id) { 
-      return { ...book , title: title } 
+      return { ...book , ...response.data } 
       }
       return book;
     })
@@ -27,17 +33,27 @@ const App = () => {
     
   }
 
-  const createBook = (title) => {
+  const createBook = async(title) => {
+    
+    const response = await axios.post( "http://localhost:3001/books" , {
+        title:title
+      } )
+    
     const updatedBooks = [
       ...books , 
-      {
-        id : Math.round( Math.random() * 9999 ) ,
-        title : title
-      }
-
+      response.data
     ]
     setBooks(updatedBooks);
   }
+
+  const fetchBook = async () => {
+    const response = await axios.get("http://localhost:3001/books")
+    setBooks(response.data); 
+  }
+
+  useEffect ( () => {
+    fetchBook()
+  } , [] )
   
   return (
    <div>
